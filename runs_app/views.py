39bc6cd4d2cb24627c07da1158e86d79d23b4206
buraizu <bryce.eadie@datadoghq.com>
2022-04-24 10:21:18 -0700

    Add and link runs_app template

--- runs_app/views.py
+++ runs_app/views.py
@@ -2,16 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.urls import reverse
 
-pages = {
-    'runs': 'List Runs',
-    'add_run': 'Add Run',
-    'run_stats': 'Running Statistics'
-}
-
 # Create your views here.
-def runs_view(request, topic):
-    try:
-        result = pages[topic]
-        return HttpResponse(pages[topic])
-    except:
-        raise Http404("404 Generic Error") # Can connect to 404 template
+def runs_view(request):
+    return render(request, 'runs_app/example.html')
+
