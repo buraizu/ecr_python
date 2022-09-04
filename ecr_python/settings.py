447665272209e661d3719d761217a4bae1127e15
buraizu <bryce.eadie@datadoghq.com>
2022-09-04 16:43:48 -0700

    Restore default settings

--- ecr_python/settings.py
+++ ecr_python/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bea7!!x3itl1w5&=tn8=)(e!9-fw2l7$0_9q46hfn+c86r*yaj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
