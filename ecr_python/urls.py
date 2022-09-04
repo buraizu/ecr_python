165d0d5e208d50af1459281cce58e7a7fdcfd41d
buraizu <bryce.eadie@datadoghq.com>
2022-09-04 15:55:50 -0700

    Add templates folder to settings

--- ecr_python/urls.py
+++ ecr_python/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+
 urlpatterns = [
     path('runs_app/', include('runs_app.urls')),
     path('admin/', admin.site.urls),
