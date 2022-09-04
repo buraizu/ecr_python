a69550841f14ab1ba37d9cab7476aa70e5cbb727
buraizu <bryce.eadie@datadoghq.com>
2022-09-04 15:54:19 -0700

    Add names for URLs in runs_app

--- runs_app/urls.py
+++ runs_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+
+# register the app namespace
+# URL NAMES
+app_name = 'runs_app'
+
 urlpatterns = [
-    path('', views.runs_view),
-    path('run/', views.run_view),
+    path('', views.runs_view, name='runs_view'),
+    path('run/', views.run_view, name='run_view'),
 ]
