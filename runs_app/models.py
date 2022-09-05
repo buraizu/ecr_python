3bcc6c61bbe60d04a0b3879c099a2dcf8bded315
buraizu <bryce.eadie@datadoghq.com>
2022-09-05 10:28:03 -0700

    Add Run model

--- runs_app/models.py
+++ runs_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Run(models.Model):
+    course = models.CharField(max_length=80)
+    distance = models.IntegerField()
+    time = models.IntegerField()
+    rating = models.IntegerField()
+    review = models.TextField()
