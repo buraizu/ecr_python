fed7b428efba12cd75af2865dc99452230243261
buraizu <bryce.eadie@datadoghq.com>
2022-09-10 17:34:25 -0700

    Add validators to Run model

--- runs_app/models.py
+++ runs_app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class Run(models.Model):
     course = models.CharField(max_length=80)
-    distance = models.IntegerField()
-    time = models.IntegerField()
-    rating = models.IntegerField()
+    distance = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(500)])
+    time = models.IntegerField(validators=[MinValueValidator(5)])
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
     review = models.TextField()
+
+    def __str__(self):
+
+        return f"{self.course} with a distance of {self.distance} and a time of {self.time}. The rating is {self.rating} and the review is {self.review}."
