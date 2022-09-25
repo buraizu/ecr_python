32af4a34390eb1bd3052e03aeffce93f94474a28
buraizu <bryce.eadie@datadoghq.com>
2022-09-25 14:31:03 -0700

    Update model and render in list template

--- events_app/models.py
+++ events_app/models.py
@@ -17,10 +17,10 @@ class UserEvent(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     event = models.ForeignKey('Event', on_delete=models.PROTECT)
     goal_time = models.IntegerField(validators=[MinValueValidator(1)])
-    result_time = models.IntegerField(validators=[MinValueValidator(1)])
+    result_time = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse("user_event_detail", kwargs={"pk": self.pk})
     
     def __str__(self):
-        return f"{self.user}'s {self.event.name} on {self.event.date}!"
+        return f"{self.event.name} on {self.event.date}!"
