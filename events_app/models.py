91d202ab25ba7d92f5a8df84b1fee48ef06cdadf
buraizu <bryce.eadie@datadoghq.com>
2022-09-25 11:36:36 -0700

    Add model for UserEvent

--- events_app/models.py
+++ events_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.urls import reverse
+from django.core.validators import MinValueValidator
+import uuid
 
 class Event(models.Model):
     name = models.CharField(max_length=500)
@@ -9,3 +12,15 @@ class Event(models.Model):
 
     def __str__(self):
         return f"{self.name} on {self.date}."
+
+class UserEvent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    event = models.ForeignKey('Event', on_delete=models.PROTECT)
+    goal_time = models.IntegerField(validators=[MinValueValidator(1)])
+    result_time = models.IntegerField(validators=[MinValueValidator(1)])
+
+    def get_absolute_url(self):
+        return reverse("user_event_detail", kwargs={"pk": self.pk})
+    
+    def __str__(self):
+        return f"{self.user}'s {self.event.name} on {self.event.date}!"
