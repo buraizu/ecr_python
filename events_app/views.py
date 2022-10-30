3224480e71e33f3044a743b6d94e91f385116b6b
buraizu <bryce.eadie@datadoghq.com>
2022-10-30 09:40:14 -0700

    Add LoginRequiredMixin

--- events_app/views.py
+++ events_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from events_app.models import Event
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class EventListView(ListView):
+class EventListView(LoginRequiredMixin, ListView):
     model = Event
     context_object_name = "event_list"
