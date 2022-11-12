f95ac9f06861ff5baa14737970dbb369bed2b511
buraizu <bryce.eadie@datadoghq.com>
2022-11-11 17:56:42 -0800

    Add form and page for creating Userevents

--- events_app/urls.py
+++ events_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import EventListView
+from .views import EventListView, UserEventCreateView
 from . import views
 
 app_name = 'events_app'
 
 urlpatterns = [
-    path('list_events', EventListView.as_view(), name='list_events')
+    path('list_events', EventListView.as_view(), name='list_events'),
+    path('create_userevent/', UserEventCreateView.as_view(), name='create_user_event')
 ]
--- events_app/views.py
+++ events_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from events_app.models import Event
+from events_app.models import Event, UserEvent
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 # Create your views here.
-class EventListView( ListView):
+class EventListView(ListView):
     model = Event
     context_object_name = "event_list"
 
@@ -14,3 +14,14 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class UserEventCreateView(CreateView):
+    model = UserEvent
+    fields = ['goal_time']
+
+    def form_valid(self, form):
+        form.instance.x = self.request.x
+
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('events_app:list_events')
