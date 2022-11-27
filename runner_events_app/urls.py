1dc8d7da64ee9ff7ed1da2f03441d96a45615378
buraizu <bryce.eadie@datadoghq.com>
2022-11-27 11:23:35 -0800

    Add RunnerEvent list view

--- runner_events_app/urls.py
+++ runner_events_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RunnerEventCreateView
+from .views import RunnerEventCreateView, RunnerEventListView
 from . import views
 
 app_name = 'runner_events_app'
 
 urlpatterns = [
-    path('create_runner_event/', RunnerEventCreateView.as_view(), name='create_runner_event')
+    path('create_runner_event/', RunnerEventCreateView.as_view(), name='create_runner_event'),
+    path('list_runner_events/', RunnerEventListView.as_view(), name='list_runner_events')
 ]
--- runner_events_app/views.py
+++ runner_events_app/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import RunnerEvent
+
 
 # Create your views here.
 class RunnerEventCreateView(CreateView, LoginRequiredMixin):
     model = RunnerEvent
     fields = ['goal_time']
 
-    # def form_valid(self, form):
-    #     form.instance.runner = self.request.user
+    def form_valid(self, form):
+        form.instance.runner = self.request.user
         # form.instance.event = 
-        # return super().form_valid(form)
+        return super().form_valid(form)
         
     # success_url = reverse_lazy('runs_app:list_runs')
+
+class RunnerEventListView(ListView, LoginRequiredMixin):
+    model = RunnerEvent
+    template_name = 'runner_events_app/runnerevent_list.html'
+
+    def get_queryset(self):
+        return RunnerEvent.objects.filter(runner=self.request.user).all()
